dfc8e9dd8be5b7c5e9a1ad86334291e8522f7660
F0X <f0xde@protonmail.com>
2020-12-03 21:48:25 +0100

    Solve day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -2,17 +2,34 @@ use aoc_runner_derive::aoc;
 
 #[aoc(day3, part1)]
 pub fn solve_part1(input: &[u8]) -> usize {
-    // width without newline
-    let width = input.iter().position(|&b| b == b'\n').unwrap();
+    check_slope(input, get_width(input), (3, 1))
+}
+
+#[aoc(day3, part2)]
+pub fn solve_part2(input: &[u8]) -> usize {
+    let width = get_width(input);
+    let mut result = 1;
+    for x in (1..=7).step_by(2) {
+        result *= check_slope(input, width, (x, 1));
+    }
+    result * check_slope(input, width, (1, 2))
+}
+
+// Width without newline
+fn get_width(input: &[u8]) -> usize {
+    input.iter().position(|&b| b == b'\n').unwrap()
+}
+
+fn check_slope(input: &[u8], width: usize, slope: (usize, usize)) -> usize {
     let mut trees = 0;
-    let mut x = 3;
-    let mut y = 1;
+    let mut x = slope.0;
+    let mut y = slope.1;
     return loop {
         match input.get(y * (width + 1) + (x % width)) {
             Some(&chr) => trees += (chr == b'#') as usize,
             None => break trees,
         }
-        x += 3;
-        y += 1;
+        x += slope.0;
+        y += slope.1;
     }
 }
