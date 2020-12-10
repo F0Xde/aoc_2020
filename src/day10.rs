1a1793931c2aa98b8f3dd6c6d99f0f37fd3c6302
F0X <f0xde@protonmail.com>
2020-12-10 13:01:27 +0100

    Use match for day 10 part 1

--- src/day10.rs
+++ src/day10.rs
@@ -5,22 +5,19 @@ pub fn gen(input: &str) -> Vec<u32> {
     let mut input: Vec<_> = input.lines().map(|num| num.parse().unwrap()).collect();
     input.sort_unstable();
     input.insert(0, 0);
-    input.push(input[input.len() - 1] + 3);
+    input.push(input.last().unwrap() + 3);
     input
 }
 
 #[aoc(day10, part1)]
 pub fn solve_part1(input: &[u32]) -> u32 {
-    let (one, three) = input.windows(2).fold((0, 0), |(one, three), win| {
-        let diff = win[1] - win[0];
-        if diff == 1 {
-            (one + 1, three)
-        } else if diff == 3 {
-            (one, three + 1)
-        } else {
-            (one, three)
-        }
-    });
+    let (one, three) = input
+        .windows(2)
+        .fold((0, 0), |(one, three), win| match win[1] - win[0] {
+            1 => (one + 1, three),
+            3 => (one, three + 1),
+            _ => (one, three),
+        });
     one * three
 }
 
